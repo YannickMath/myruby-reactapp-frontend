b237dc942202f0348e51104d8d4d089f58ef5a8a
Yannick.Mathieu <yannick.mathieu@toolib.fr>
2023-07-04 10:59:09 +0200

    installation redux toolkit et sagas

---
+++ src/sagas/index.js
@@ -0,0 +1,8 @@
+
+export default function* rootSaga() {
+    // yield all([ 
+        // Utilisez la fonction all() pour exécuter plusieurs sagas en parallèle
+        // Ajoutez ici les sagas
+    // ]);
+    }
+
