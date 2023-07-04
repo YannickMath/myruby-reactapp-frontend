9a2961d16274d1865ccd787c2fd00bf5cdc87537
Yannick.Mathieu <yannick.mathieu@toolib.fr>
2023-07-04 10:10:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import { Provider } from 'react-redux'; // Importez le composant Provider de react-redux
+import rootReducer from './reducers'; // Importez le reducer
+import rootSaga from './sagas'; // Importez le saga
+
+const sagaMiddleware = createSagaMiddleware(); //installation de redux-saga
+
+const store = configureStore({ //création du store
+  reducer: rootReducer,
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(rootSaga); //lancement de redux-saga
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}> {/* Ajoutez le composant Provider et fournissez le store */}
+      <div className="App">
+        BONJOUR !
+      </div>
+    </Provider>
   );
 }
 
